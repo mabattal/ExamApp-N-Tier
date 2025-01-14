150670f95003325e2125fa2fbe4451cdeb4c87f3
MEHMET ALİ BATTAL <mehmetalibattal@hotmail.com>
2025-01-14 22:23:28 +0300

    Repository Design Pattern uygulaması

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ExamApp.Repositories
+{
+    public class GenericRepository<T>(AppDbContext context) : IGenericRepository<T> where T : class
+    {
+        private readonly DbSet<T> _dbSet = context.Set<T>();
+
+        public IQueryable<T> GetAll()     
+        {
+            return _dbSet.AsQueryable().AsNoTracking();
+        }
+
+        //tek satırda return edilebilecek bir ifade olduğu için süslü parantez kullanmadık. Lambda ile yazdık.
+        public IQueryable<T> Where(Expression<Func<T, bool>> predicate) => _dbSet.Where(predicate).AsNoTracking();
+
+        public ValueTask<T?> GetByIdAsync(int id) => _dbSet.FindAsync(id);
+
+        public async ValueTask AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+        public void Update(T entity) => _dbSet.Update(entity);
+
+        public void Delete(T entity) => _dbSet.Remove(entity);
+    }
+}
