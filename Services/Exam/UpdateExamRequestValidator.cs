c639e537dda23b3001b85b002d72e7f3601cd191
MEHMET ALİ BATTAL <mehmetalibattal@hotmail.com>
2025-02-24 01:02:50 +0300

    Add validators for exam creation and update requests
    Implemented `CreateExamRequestValidator` and `UpdateExamRequestValidator` classes in the `ExamApp.Services.Exam` namespace. These validators enforce rules for properties like `Title`, `Description`, `StartDate`, `EndDate`, `Duration`, and `CreatedBy` to ensure valid input for exam requests.

---
+++ Services/Exam/UpdateExamRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+
+namespace ExamApp.Services.Exam
+{
+    public class UpdateExamRequestValidator : AbstractValidator<UpdateExamRequestDto>
+    {
+        public UpdateExamRequestValidator()
+        {
+            RuleFor(x=> x.Title)
+                .NotEmpty().WithMessage("Title required.")
+                .MaximumLength(200).WithMessage("Title must not exceed 200 characters.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description required.")
+                .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+
+            RuleFor(x => x.StartDate)
+                .NotEmpty().WithMessage("StartDate required.");
+
+            RuleFor(x => x.EndDate)
+                .NotEmpty().WithMessage("EndDate required.");
+
+            RuleFor(x => x.Duration)
+                .GreaterThan(0).WithMessage("Duration required and must be greater than 0.");
+
+            RuleFor(x => x.CreatedBy)
+                .NotEmpty().WithMessage("CreatedBy required.");
+        }
+    }
+}
